4a91394ef5a8c89bd34e0b607b6585cae05e3f24
Travis Craft <tlcraft@users.noreply.github.com>
2019-01-18 19:16:25 -0700

    Create initial boilerplate TDD project

---
+++ Tests/IntroToTDDTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tests
+{
+    public class IntroToTDDTests
+    {
+        [Fact]
+        public void Test_BestIntroToTDD()
+        {
+            Assert.True(false);
+        }
+    }
+}
